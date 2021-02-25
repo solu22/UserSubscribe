d026c0e4c395f9eceb769306c9762f193a37cab7
sherpa27 <pasangnurushrp8@gmail.com>
2021-02-25 19:09:20 +0200

    change in success all

--- app.js
+++ app.js
@@ -65,6 +65,10 @@ app.post("/failure", (req, res)=>{
     res.redirect("/");
 })
 
+app.post("/", (req, res)=>{
+    res.redirect("/");
+})
+
 app.listen(process.env.PORT || 3000 ,()=>{
     console.log("Server is running on port 3000");
 });
